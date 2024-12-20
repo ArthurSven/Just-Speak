0f3c20c9000851792f06dfcb6b7cb4376dbb38ac
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2024-12-20 12:36:28 +0200

    The project is only being laid out for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule} from "@angular/core";
+import path from "node:path";
+import {DefaultLayoutComponent} from "./layouts/default-layout/default-layout.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  }
+]
