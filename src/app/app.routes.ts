5468fc5724bf9fc054d4ce3c08c299877b6b6a31
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2025-01-02 21:23:36 +0200

    Started working on the German section, customising the sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { NgModule} from "@angular/core";
 import path from "node:path";
 import {DefaultLayoutComponent} from "./layouts/default-layout/default-layout.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {GermanLayoutComponent} from "./layouts/german-layout/german-layout.component";
 
 export const routes: Routes = [
   {
@@ -10,6 +11,13 @@ export const routes: Routes = [
     component: DefaultLayoutComponent,
     children: [
       {path: '', component: HomeComponent}
+    ],
+  },
+  {
+    path: 'german',
+    component: GermanLayoutComponent,
+    children: [
+
     ]
   }
 ]
